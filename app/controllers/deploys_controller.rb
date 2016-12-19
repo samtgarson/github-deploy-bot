7146c1abaa09d1cb6215400bbe44b0f8ad31b86b
Sam Garson <samtgarson@gmail.com>
2016-12-19 11:52:03 +0000

    Basic auth

--- app/controllers/deploys_controller.rb
+++ app/controllers/deploys_controller.rb
@@ -1,4 +1,6 @@
 class DeploysController < ApplicationController
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+
   def create
     head 200 and return if result.success?
     render json: result.errors, status: 500
