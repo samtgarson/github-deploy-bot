dd80d23ba5708eac9d53441ca78ece03b24b15f0
Sam Garson <samtgarson@gmail.com>
2016-12-19 10:58:19 +0000

    Basic post comment interactor

--- app/interactors/post_comment.rb
+++ app/interactors/post_comment.rb
@@ -2,5 +2,25 @@ class PostComment
   include Interactor
 
   def call
+    success = post_comment!
+    contex.fail!(error: 'Something went wrong') unless success
   end
+
+  private
+
+    def pull_requests
+      @pull_requests ||= Octokit.pull_requests(context.repo, state: 'open')
+    end
+
+    def pull_request
+      @issue ||= pull_requests.detect { |pr| pr[:head][:ref] == context.branch }
+    end
+
+    def post_comment!
+      Octokit.add_comment(context.repo, pull_request[:number], message)
+    end
+
+    def message
+      @message ||= context.message || "**Deployed to:**\n#{context.url}"
+    end
 end
